e9e946c33c9f7a4687370c37f70360f32deb62ca
Benjamin Bouvier <public@benj.me>
2018-08-03 15:31:15 +0200

    Remove the atomic initialization to avoid a race;
    and gracefully handle the case where a logger has been already set up;

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,10 @@ use std::cell::RefCell;
 use std::env;
 use std::fs::File;
 use std::io::{self, Write};
-use std::sync::atomic;
 use std::thread;
 
 use env_logger::filter::{Builder, Filter};
 use log::{LevelFilter, Metadata, Record};
-
-static INITIALIZED: atomic::AtomicBool = atomic::AtomicBool::new(false);
 
 thread_local! {
     static WRITER: RefCell<Option<io::BufWriter<File>>> = RefCell::new(None);
@@ -30,20 +27,16 @@ pub fn initialize(filename_prefix: &str) {
         builder.build()
     });
 
-    if level_filter.is_some() {
-        // Ensure the thread local state is always properly initialized.
-        WRITER.with(|rc| {
-            if rc.borrow().is_none() {
-                rc.replace(Some(open_file(filename_prefix)));
-            }
-        });
-    }
-
-    if INITIALIZED.load(atomic::Ordering::Relaxed) || level_filter.is_none() {
+    if level_filter.is_none() {
         return;
     }
 
-    INITIALIZED.store(true, atomic::Ordering::Relaxed);
+    // Ensure the thread local state is always properly initialized.
+    WRITER.with(|rc| {
+        if rc.borrow().is_none() {
+            rc.replace(Some(open_file(filename_prefix)));
+        }
+    });
 
     let logger = FilePerThreadLogger::new(level_filter.unwrap());
     let setup_result =
@@ -51,7 +44,7 @@ pub fn initialize(filename_prefix: &str) {
     match setup_result {
         Ok(_) => {}
         Err(_) => {
-            warn!("Another logger has been set up before the file-per-thread logger, aborting.");
+            // Another logger has been set up, but log can handle this.
         }
     }
 
@@ -70,7 +63,7 @@ impl FilePerThreadLogger {
 
 impl log::Log for FilePerThreadLogger {
     fn enabled(&self, metadata: &Metadata) -> bool {
-        enabled() && self.filter.enabled(metadata)
+        self.filter.enabled(metadata)
     }
 
     fn log(&self, record: &Record) {
@@ -94,12 +87,6 @@ impl log::Log for FilePerThreadLogger {
             let _ = writer.flush();
         });
     }
-}
-
-/// Checks whether the logging state has ever been initialized or not.
-#[inline]
-fn enabled() -> bool {
-    INITIALIZED.load(atomic::Ordering::Relaxed)
 }
 
 /// Open the tracing file for the current thread.
